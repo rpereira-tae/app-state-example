1d6b1f40606a86f3d32d5eee50e707d36bf3fa8f
Ronald Pereira <rpereira@taecontrol.com>
2023-04-24 14:24:04 -0400

    Set and retrieve state with AppHandler and Tauri::Manager

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,7 +2,11 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use std::sync::Mutex;
-use tauri::State;
+use tauri::{App, State};
+
+use tauri::Manager;
+
+struct Name(String);
 
 struct Counter {
     count: Mutex<i32>,
@@ -15,11 +19,11 @@ fn calculate(method: &str, state: State<Counter>) -> i32 {
     match method {
         "add" => {
             *counter = *counter + 1;
-        },
+        }
         "subtract" => {
             *counter = *counter - 1;
-        },
-        _ => ()
+        }
+        _ => (),
     }
 
     *counter
@@ -27,7 +31,23 @@ fn calculate(method: &str, state: State<Counter>) -> i32 {
 
 fn main() {
     tauri::Builder::default()
-        .manage(Counter { count: Mutex::new(0) })
+        .setup(|app: &mut App| {
+            app.manage(Name("Counter App".into()));
+
+            let app_name: State<Name> = app.state();
+
+            let counter_state: State<Counter> = app.state();
+
+            let counter = counter_state.count.lock().unwrap();
+
+            assert_eq!(app_name.0, "Counter App");
+            assert_eq!(*counter, 0);
+
+            Ok(())
+        })
+        .manage(Counter {
+            count: Mutex::new(0),
+        })
         .invoke_handler(tauri::generate_handler![calculate])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
